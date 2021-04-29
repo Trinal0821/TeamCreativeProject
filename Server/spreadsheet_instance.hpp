38a90d9ffbbebbe590c38a5d7d90b5640c6e1f9a
NATHAN ROSKELLEY <u1095732@umail.utah.edu>
2021-04-29 09:01:58 -0600

    Added XML interpritation, and now writes changes to files

--- Server/spreadsheet_instance.hpp
+++ Server/spreadsheet_instance.hpp
@@ -2,12 +2,14 @@
 #include <cstdlib>
 #include <deque>
 #include <iostream>
+#include <fstream>
 #include <list>
 #include <set>
 #include <boost/bind.hpp>
 #include <boost/shared_ptr.hpp>
 #include <boost/enable_shared_from_this.hpp>
 #include <boost/asio.hpp>
+#include "rapidxml/rapidxml.hpp"
 
 class spreadsheet_instance
 {
@@ -17,10 +19,11 @@ public:
 		Creates an instance of the spreadsheet
 	*/
 	spreadsheet_instance(std::string filename){
-			std::list<std::string> fileList;
-		    std::string path = "Spreadsheet";
-		    for (const auto & entry : std::filesystem::directory_iterator(path))
-		        fileList.push_back(entry.path());
+		this.filename="Spreadsheet/"+filename+".sprd";
+		std::list<std::string> fileList;
+    std::string path = "Spreadsheet";
+    for (const auto & entry : std::filesystem::directory_iterator(path))
+        fileList.push_back(entry.path());
 
 		*SpreadsheetObject foundSheet = null;
 		for(sheet = fileList.begin; sheet!=fileList.end(); ++sheet){
@@ -32,10 +35,27 @@ public:
 
 		//If it doesn't exist, create it
 		if(foundSheet==null){
-			ofstream spreadsheetFile("Spreadsheet/"+filename+".xls");
-			foundSheet = new Spreadsheet(spreadsheet)
-			allSpreadsheets.push_back(foundSheet);
-			spreadsheetStrings.push_back(spreadsheet);
+			ofstream spreadsheetFile("Spreadsheet/"+filename+".sprd");
+			spreadsheetFile <<"<Spreadsheet>\n";
+			spreadsheetFile <<"</Spreadsheet>\n";
+			spreadsheetFile.close();
+		}
+
+		//If it does, put the cells in the actions newNode
+		else{
+			rapidxml::file<> xmlFile("Spreadsheet/"+filename+".sprd"); // Default template is char
+	    rapidxml::xml_document<> doc;
+	    doc.parse<0>(xmlFile.data());
+
+			rapidxml::xml_node<> * root_node = doc.first_node("Spreadsheet");
+			rapidxml::xml_node<> * cell = NULL;
+			rapidxml::xml_node<> * contents = NULL;
+			for (rapidxml::xml_node<> * node = root_node->first_node("Cell"); node; node = node->next_sibling()){
+				cell = node->first_node("Cell");
+				contents = node->first_node("Contents");
+				this.actions.push_back(new ActionNode(cell,contents));
+			}
+
 		}
 
 		//TODO: Send the data to the client
@@ -43,6 +63,10 @@ public:
 		//If there is an error, we will return a string containing the error
 		return null;
 	}
+
+
+
+
 	/*
 	* Assigns the client to the instance of the server, for output purposes
 	*/
@@ -52,6 +76,9 @@ public:
 		client.deliver();
 	}
 
+
+
+
 	/*
 	* Remove a client from the list of connected clients
 	*/
@@ -60,33 +87,83 @@ public:
 		clients_.erase(client);
 	}
 
+
+
+
 	/*
 	* Send a message to all clients
 	*/
 	void deliver(const std::string& msg)
 	{
-		std::for_each(clients_.begin(), clients_.end(),
-			boost::bind(&client::deliver, _1, boost::ref(msg)));
+		std::for_each(participants_.begin(), participants_.end(),
+			boost::bind(&chat_participant::deliver, _1, boost::ref(msg)));
 	}
 
+
+
+
 	/*
-		TODO: Undo a cell action
+		Undo a cell action
 	*/
 	void undo(){
-
+		ActionNode removingNode = actions.pop();
+		for(node = actions.begin; node!=actions.end(); ++node){
+			if(removingNode.cell = node.cell){
+				addCell(removingNode.cell, removingNode.value);
+				undone.push_back(new ActionNode(removingNode.cell, removingNode.value));
+				return;
+			}
+		}
+		addCell("", removingNode.value);
+		undone.push_back(new ActionNode(removingNode.cell, removingNode.value));
 	}
 
+
+
+
 	/*
-		TODO: Redo a cell action
+		Redo a cell action
 	*/
 	void redo(){
-
+		ActionNode newNode = undone.pop();
+		addCell(newNode.cell, newNode.value);
 	}
 
+
+
+
+
 	/*
-		TODO: Update a cell in the file
+		Update a cell in the file
 	*/
 	void addCell(std::string cell, std::string value){
+
+		//This is the worst way to do this, but I'm running out of time
+		remove(this.filename);
+
+		ofstream spreadsheetFile(this.filename);
+		spreadsheetFile << "<Spreadsheet>\n";
+		for(node = actions.begin; node!=actions.end(); ++node){
+			spreadsheetFile << "<Name>" << node->cell << "</Name>\n";
+			spreadsheetFile << "<Contents>" << node->contents << "</Contents>\n";
+		}
+		spreadsheetFile << "</Spreadsheet>\n";
+
+		spreadsheetFile.close();
+
+		actions.push_back(new ActionNode(cell, value));
+		std::string message = "{\"messageType\":\"editCell\", \"cellName\": \""+cell+"\",\"contents\":\""+update+"\"}";
+		broadcastToAll(message);
+	}
+
+
+
+
+
+	/*
+		TODO: Broadcast a message to all the users
+	*/
+	void broadcastToAll(std::string message){
 
 	}
 
@@ -95,6 +172,9 @@ private:
 	std::set<client_ptr> clients_;
 	enum { max_recent_msgs = 100 };
 	message_queue messages_;
+	list<ActionNode> actions;
+	list<ActionNode> undone;
+	std::string filename;
 };
 
 typedef std::deque<chat_message> message_queue;
